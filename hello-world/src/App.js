e26ee132af47a0afd53ad3a40d9608d76bb95dc9
swathicherala <swathicherala96@gmail.com>
2026-03-07 16:10:43 +0530

    jsx project

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const pets = [
+    {
+      name: "Dolly",
+      description: "Doll is funny and loves to be around",
+      skills: ["fetch","roll over","bark"],
+      image:"https://res.cloudinary.com/dsrdpxrjh/image/upload/v1772878864/dog2_dugafj.jpg"
+    },
+    {
+      name: "Moll",
+      description: "Moll is funny and loves to be around",
+      skills: ["cuddle","roll over","bark"],
+      image:"https://res.cloudinary.com/dsrdpxrjh/image/upload/v1772878864/dog1_akall2.jpg"
+    },
+    {
+      name: "Dolly",
+      description: "Doll is funny and loves to be around",
+      skills: ["fetch","roll over","bark"],
+      image:"https://res.cloudinary.com/dsrdpxrjh/image/upload/v1772878864/dog2_dugafj.jpg"
+    },
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Pets</h1>
+      <ul className="container">
+        {
+          pets.map((pet,index)=>(
+            <li key={index}>
+              <h1>{pet.name}</h1>
+              <p>{pet.description}</p>
+              <img src={pet.image} alt={pet.name} width="200px" height="100px"/>
+              <ul className="skills">
+                {
+                  pet.skills.map((skill,index)=>(
+                    <li key={skill}>{skill}</li>
+                  ))
+                }
+              </ul>
+            </li>
+          ))
+        }
+      </ul>
     </div>
   );
 }
