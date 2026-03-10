5c939ff8f2894c8e86f2f2d018c9122df2338505
swathicherala <swathicherala96@gmail.com>
2026-03-10 21:14:23 +0530

    fetch api wt loading and async and await

--- hello-world/src/FetchApi.js
+++ hello-world/src/FetchApi.js
@@ -2,19 +2,42 @@ import { useEffect, useState } from "react"
 
 const FetchApi = () => {
     const [users,setUsers] = useState([])
-    useEffect(() =>{
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then((res)=>res.json())
-        .then((data)=>{
+    const [loading, setLoading] = useState(true)
+    // useEffect(() =>{
+    //     fetch('https://jsonplaceholder.typicode.com/users')
+    //     .then((res)=>res.json())
+    //     .then((data)=>{
+    //         setUsers(data)
+    //     })
+    //     .catch((err)=>{
+    //         console.log(err)
+    //     })
+    // },[])
+
+    //Using Async and Await
+    const fetchUsers = async() => {
+        try{
+            const response = await fetch('https://jsonplaceholder.typicode.com/users')
+            const data = await response.json()
             setUsers(data)
-        })
-        .catch((err)=>{
-            console.log(err)
-        })
+        }catch(err){
+            console.log('Error fetching users', err)
+        }finally{
+            setLoading(false)
+        }
+    }
+
+    useEffect(()=>{
+        fetchUsers()
     },[])
+
+    if(loading){
+        return <h3>Loading......</h3>
+    }
+
     return (
         <div>
-            <h1>Users</h1>
+            <h1>User List</h1>
             <ul>
                 {
                     users.map((user)=>(
